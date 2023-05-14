b9b12a8981dd95169278129b1f9c8f29196cd70f
Santiago Pérez Gómez <spergo@outlook.com>
2023-05-14 11:24:53 -0600

    front-contact-feat: alert and emailjs service

--- Frontend/assets/js/contact.js
+++ Frontend/assets/js/contact.js
@@ -1,27 +1,27 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+$(document).ready(() => {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = $('#contact-form');
+
+  // Preparing emailJS service
+  emailjs.init('XUqOS_pXRgggqCAW9');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+        form.classList.add('was-validated');
+      } else {
+        try {
+          emailjs.sendForm('petzonalize_service', 'petzonalize_template', form);
+          sessionStorage.setItem("email-status", "true");
+          sessionStorage.setItem("email-operation", "¡Correo Enviado con Éxito!");
+        } catch (error) {
+          sessionStorage.setItem("email-status", "false");
+          sessionStorage.setItem("email-operation", "¡Hubo un Error al Enviar el Correo!");
         }
-         else{
-          setTimeout(()=>{window.location.href = "https://petzonalize.netlify.app/";}, 2500)
-          alert("Gracias por su opinion")
-          
-         
-            }
-           
-  
-        form.classList.add('was-validated')
-        
-      }, false)
-    })
-  })()
+      }
+    }, false);
+  })
+});
---
+++ Frontend/assets/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+    const emailResult = sessionStorage.getItem("email-operation");
+    const emailStatus = sessionStorage.getItem("email-status");
+
+    const alertElement = $("#alert");
+    alertElement.hide();
+
+    if(emailResult !== null && emailStatus !== null) {
+        alertElement.text(emailResult);
+        alertElement.slideDown(250);
+        setTimeout(() => alertElement.slideUp(250, () => $(this).remove()), 5000);
+
+        if(emailStatus === "false") {
+            alertElement.removeClass("alert-success");
+            alertElement.removeClass("text-success");
+            alertElement.addClass("alert-danger");
+            alertElement.addClass("text-danger");
+        }
+    }
+});
