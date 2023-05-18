065d9a51423b207d379fdc1d78179aec77b7ec18
Iván <ivanalex081195@gmail.com>
2023-05-18 17:04:25 -0600

    front-productdettails-feat: added colors selection

--- Frontend/assets/js/product-details.js
+++ Frontend/assets/js/product-details.js
@@ -31,3 +31,10 @@ $(document).ready(() => {
     
   }
 });
+
+let input = document.querySelector(".circle");
+input.addEventListener("mouseover", aplicarFocus);
+
+function aplicarFocus() {
+  input.focus();
+}
