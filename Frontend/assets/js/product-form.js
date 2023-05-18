6b9e026e3319551f6f9577e1e075d9970dab04f9
Saidgp <said.gar.p@gmail.com>
2023-05-18 12:52:11 -0600

    front-index-fix: merge conflict

--- Frontend/assets/js/product-form.js
+++ Frontend/assets/js/product-form.js
@@ -1,5 +1,22 @@
+//Obtener el json y cargarlo a local storage 
 fetch("/assets/json/products.json")
 .then(res => res.json())
 .then(data =>{
     console.log(data)
-})
+    const products = data.products;
+   
+    //let actualProducts=JSON.stringify(products);
+    //localStorage.setItem("products", actualProducts);
+});
+
+//obtener los datos del formulario
+
+let productos= localStorage.getItem("products")
+productos=JSON.parse(productos);
+console.log("productos", productos)
+let i = productos.push({id:13, name:"prueba", description:"still prueba"});
+console.log(i);
+console.log(productos)
+
+
+
--- Frontend/assets/js/profile.js
+++ Frontend/assets/js/profile.js
@@ -201,88 +201,24 @@ fetch(urlUsers)
     document.getElementById('email').value = user.email;
     document.getElementById('phone').value = user.phone;
     document.getElementById('password').value = user.password;
+
+    alertElement.text("Cuenta eliminada");
+    alertElement.slideDown(250);
+
+    localStorage.clear();
+    sessionStorage.setItem("not-account", "true");
+
+    location.assign("../../index.html")
+
   });
 
 //Obtiene los privilegios del id del usuario
 
-fetch(urlUsers)
-  .then(response => response.json())
-  .then(data => {
-    const usuariosJson = data; // Aquí se obtiene el JSON de la respuesta
-    const referenciaUsuario = document.getElementById("estado");
-    const referenciaBoton = document.getElementById("boton-admin");
-    let estadoAdmin;
-
-    // Obtener los privilegios del usuario desde el JSON
-    const usuario = usuariosJson.users.find(user => user.id === 1);
-    if (usuario && usuario.privileges === "admin") {
-      estadoAdmin = true;
-    } else {
-      estadoAdmin = false;
-    }
-
-    if (estadoAdmin) {
-      referenciaUsuario.textContent = "Administrador";
-      referenciaBoton.style.display = "block";
-    } else {
-      referenciaUsuario.textContent = "";
-     
-    }
-  })
-  .catch(error => {
-    console.error("Error al obtener el JSON:", error);
-  });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    // Aquí va tu código JavaScript
+  
+  
+  }
     
-    // Obtener todos los botones de edición y el botón de guardar
-    const editButtons = document.querySelectorAll('.btn-edit');
-    const saveButton = document.querySelector('.btn-save');
-    const refForm =document.forms["edit-form"];
   
-    // Agregar el controlador de eventos a cada botón de edición
-    editButtons.forEach(button => {
-      button.addEventListener('click', event => {
-        event.preventDefault(); // Evitar la acción predeterminada del botón
-        // Obtener el campo asociado al botón de edición
-        const fieldId = button.getAttribute('data-field');
-        const field = document.getElementById(fieldId);
-  
-        // Habilitar la edición del campo
-        field.removeAttribute('readonly');
-        field.focus(); // Opcionalmente, poner el foco en el campo editado
-      });
-    });
-  
-    // Agregar el controlador de eventos al botón de guardar
-    refForm.addEventListener(`submit`, (event)=>{
-      event.preventDefault();
- //Recuperar la sessionstorage
-     let  user = localStorage.getItem("users");
-      let users= JSON.parse(user);
-    
-      const name =refForm.elements["name"];
-      const email =refForm.elements["email"];
-      const phone =refForm.elements["phone"];
-      const password =refForm.elements["password"];
-
-      document.getElementById('name').value=name.value;
-
-      users.name=name.value;
-      users.email=email.value;
-      users.phone=phone.value;
-      users.password=password.value;
-
-      let userActual=JSON.stringify(users);
-      sessionStorage.setItem("users", userActual);
-      localStorage.setItem("users",userActual);
-
-  
-  
-  });
-    
-  });
   
   let exit = document.getElementById(`exit`);
 
@@ -290,7 +226,7 @@ fetch(urlUsers)
     localStorage.clear();
 
   })
-    }
+    
   
 
 
