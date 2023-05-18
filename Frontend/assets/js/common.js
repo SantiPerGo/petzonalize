d2d7f260305eb08a32bd24283ea2fb37a31c983d
Santiago Pérez Gómez <spergo@outlook.com>
2023-05-18 10:44:13 -0600

    front-navbar-fix: img and icons responsive size

--- Frontend/assets/js/common.js
+++ Frontend/assets/js/common.js
@@ -1,5 +1,19 @@
 // Jquery to load navbar and footer
 $(document).ready(function(){
-    $.get("/assets/html/navbar.html", data => $("nav").replaceWith(data));
+    $.get("/assets/html/navbar.html", data => {
+        $("nav").replaceWith(data);
+
+        // Updating shopping cart counter
+        const shoppingCart = localStorage.getItem("shopping-cart");
+        if(shoppingCart !== null) {
+            const products = JSON.parse(shoppingCart);
+            const shoppingCartCounter = $("#shopping-cart-counter");
+            shoppingCartCounter.text(products.length);
+            shoppingCartCounter.removeClass("d-none");
+    
+            console.log(shoppingCartCounter)
+        }
+    });
+    
     $.get("/assets/html/footer.html", data => $("footer").replaceWith(data));
 });
