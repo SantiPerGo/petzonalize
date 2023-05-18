37cada964d0c1e3e89ea6cc1a60e5050743faa76
Alejandro Montellano <ualexmont@gmail.com>
2023-05-18 17:17:14 -0600

    frontend-shopping-cart-feat: Adding JS to show products of user from Localstorage

--- Frontend/assets/js/shopping-cart.js
+++ Frontend/assets/js/shopping-cart.js
@@ -1,2 +1,56 @@
-const userCartProducts = JSON.parse(localStorage.getItem("shopping-cart"));
-console.log(userCartProducts);
+const productsListContainer = document.getElementById("products-list");
+
+//-----------Defincion de funcion ----------------------
+const getUserProductsInStorage = () => {
+    
+    const userCartProducts = JSON.parse(localStorage.getItem("shopping-cart"));
+    console.log(userCartProducts);
+
+    let product = ``;
+    //------ Iteración de lista de productos --------------
+    for(let index = 1; index < userCartProducts.length; index++){
+        console.log(userCartProducts[index].name);
+       
+        // ----- Visualizacion de producto --------
+        product = `  <----- HTML de producto ---->     `;
+
+        productsListContainer.innerHTML += product;
+    }
+}
+
+getUserProductsInStorage();
+
+//---------------- ejemplo ----------------------------------------------
+let product = `<!------- Item ${index} ------->
+        <div class="product-box my-4 bg-beige">
+           <div class="row justify-content-center">
+            <!--------- Columna de Imagen Producto--------->
+            <div class="col col-lg-4">
+                <div>
+                    <img src="${userCartProducts[index].imgUrl}" alt="">
+                </div>
+            </div>
+            <!------- Columna Informacion de Producto-------->
+            <div class="col col-lg-8">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-12">
+                        <h4>${ userCartProducts[index].name }</h4>
+                    </div>
+                    <div class="col col-lg-12">
+                        <p>${userCartProducts[index].description}</p>
+                    </div>
+                    <div class="col-12 col-lg-12">
+                        <div class="row justify-content-center">
+                            <div class="col col-lg-6 col-sm-12">
+                                <p>Precio Individual: $<span>${userCartProducts[index].price}</span></p>
+                            </div>
+                            <div class="col col-lg-6 col-sm-12">
+                                <p>Cantidad <span>${userCartProducts[index].amount}</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+            </div>
+        </div>`;
