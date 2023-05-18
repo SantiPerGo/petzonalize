3a453093040711acab2db810e49151424fed32e4
Iván <ivanalex081195@gmail.com>
2023-05-18 12:19:07 -0600

    front-productdetails-refact: reordered html and js file

--- Frontend/assets/js/product-details.js
+++ Frontend/assets/js/product-details.js
@@ -7,6 +7,13 @@ $(document).ready(() => {
   const product = JSON.parse(sessionStorage.getItem("product"));
 
   if (product !== undefined && product !== null) {
+    if (product.customizable === true){
+      $("#product-custom").removeClass("d-none");
+      $("#product-not-custom").addClass("d-none");
+    } else {
+      $("#product-custom").addClass("d-none");
+      $("#product-not-custom").removeClass("d-none");
+    };
     $("#product-img").attr("src", product.imgUrl);
     $("#product-img").attr("alt", product.name);
     $("#product-name").text(product.name);
@@ -15,33 +22,6 @@ $(document).ready(() => {
       $("#product-description").text(product.description);
   } else {
     console.error("product doesnt exist");
-  }
-});
-
-$(document).ready(() => {
-  const product = JSON.parse(sessionStorage.getItem("product"));
-  if (product.customizable === true) {
-    document.getElementById("material-row").style.visibility = "visible";
-  } else {
-    document.getElementById("material-row").style.visibility = "hidden";
-
-  }
-});
-$(document).ready(() => {
-  const product = JSON.parse(sessionStorage.getItem("product"));
-  if (product.customizable === true) {
-    document.getElementById("size-row").style.visibility = "visible";
-  } else {
-    document.getElementById("size-row").style.visibility = "hidden";
-
-  }
-});
-$(document).ready(() => {
-  const product = JSON.parse(sessionStorage.getItem("product"));
-  if (product.customizable === true) {
-    document.getElementById("color-row").style.visibility = "visible";
-  } else {
-    document.getElementById("color-row").style.visibility = "hidden";
-
+    
   }
 });
