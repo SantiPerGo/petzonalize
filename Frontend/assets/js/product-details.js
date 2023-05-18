42e24670e171f1beda97c92422710b0a17b1f8da
Iván <ivanalex081195@gmail.com>
2023-05-18 02:22:26 -0600

    front-product-form-fix: fixed commit changes

--- Frontend/assets/js/product-details.js
+++ Frontend/assets/js/product-details.js
@@ -4,16 +4,26 @@
 
 // Loading product data into the page
 $(document).ready(() => {
-    const product = JSON.parse(sessionStorage.getItem("product"));
-    
-    if(product !== undefined && product !== null) {
-        $("#product-img").attr('src', product.imgUrl);
-        $("#product-img").attr('alt', product.name);
-        $("#product-name").text(product.name);
+  const product = JSON.parse(sessionStorage.getItem("product"));
 
-        if(product.customizable === false)
-            $("#product-description").text(product.description);
-    } else {
-        console.error("product doesnt exist");
-    }
+  if (product !== undefined && product !== null) {
+    $("#product-img").attr("src", product.imgUrl);
+    $("#product-img").attr("alt", product.name);
+    $("#product-name").text(product.name);
+
+    if (product.customizable === false)
+      $("#product-description").text(product.description);
+  } else {
+    console.error("product doesnt exist");
+  }
+});
+
+$(document).ready(() => {
+  const product = JSON.parse(sessionStorage.getItem("product"));
+  if (product.customizable === true) {
+    document.getElementById("material-row").style.visibility = "visible";
+  } else {
+    document.getElementById("material-row").style.visibility = "hidden";
+
+  }
 });
