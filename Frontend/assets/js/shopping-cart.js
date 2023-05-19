58413a825dc3f55668b26cb144af8dbf616bd6ac
Luisin <luis.enrique.sanper@gmail.com>
2023-05-19 10:56:25 -0600

    front-shoppingcart-feat: add styles products

--- Frontend/assets/js/shopping-cart.js
+++ Frontend/assets/js/shopping-cart.js
@@ -12,7 +12,47 @@ const getUserProductsInStorage = () => {
         console.log(userCartProducts[index].name);
        
         // ----- Visualizacion de producto --------
-        product = `  <----- HTML de producto ---->     `;
+        product = `<div class="product-box my-4 bg-beige">
+        <div class="row justify-content-center">
+         <!--------- Columna de Imagen Producto--------->
+         <div class="col col-lg-4 productimagecontainer">
+             <div>
+                 <img class="productimage" src=${userCartProducts[index].imgUrl} alt="">
+             </div>
+         </div>
+         <!------- Columna Informacion de Producto-------->
+         <div class="col col-lg-8">
+             <div class="row justify-content-center">
+                 <div class="col-12 col-lg-12">
+                     <h4>${ userCartProducts[index].name }</h4>
+                 </div>
+                 <div class="col col-lg-12 descriptioncontainer">
+                     <p>${userCartProducts[index].description}</p>
+                 </div>
+                 <div class="col-12 col-lg-12">
+                     <div class="row justify-content-center">
+                         <div class="col col-lg-6 col-sm-12">
+                             <p>Precio Individual: $<span>${userCartProducts[index].price}</span></p>
+                         </div>
+                         <div class="col col-lg-6 col-sm-12">
+                             <div class="container">
+                                 <div class="row">
+                             <p id="cantidadproductos">
+                                 Cantidad
+                                 <button type="button" onclick="" class="button-icon"><ion-icon id="removepiece" name="remove-circle"></ion-icon></button>
+                                 <span class="number-display">${userCartProducts[index].amount}</span>
+                                <button type="button" onclick="" class="button-icon"><ion-icon id="addpiece" name="add-circle"></ion-icon></button>
+                             </p>
+                         </div>
+                         </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+        
+         </div>
+     </div>     `;
 
         productsListContainer.innerHTML += product;
     }
@@ -20,37 +60,3 @@ const getUserProductsInStorage = () => {
 
 getUserProductsInStorage();
 
-//---------------- ejemplo ----------------------------------------------
-let product = `<!------- Item ${index} ------->
-        <div class="product-box my-4 bg-beige">
-           <div class="row justify-content-center">
-            <!--------- Columna de Imagen Producto--------->
-            <div class="col col-lg-4">
-                <div>
-                    <img src="${userCartProducts[index].imgUrl}" alt="">
-                </div>
-            </div>
-            <!------- Columna Informacion de Producto-------->
-            <div class="col col-lg-8">
-                <div class="row justify-content-center">
-                    <div class="col-12 col-lg-12">
-                        <h4>${ userCartProducts[index].name }</h4>
-                    </div>
-                    <div class="col col-lg-12">
-                        <p>${userCartProducts[index].description}</p>
-                    </div>
-                    <div class="col-12 col-lg-12">
-                        <div class="row justify-content-center">
-                            <div class="col col-lg-6 col-sm-12">
-                                <p>Precio Individual: $<span>${userCartProducts[index].price}</span></p>
-                            </div>
-                            <div class="col col-lg-6 col-sm-12">
-                                <p>Cantidad <span>${userCartProducts[index].amount}</span></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-           
-            </div>
-        </div>`;
