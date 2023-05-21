4855cf8dcf83e677f24e8532545988020ac8705d
Santiago Pérez Gómez <spergo@outlook.com>
2023-05-20 21:28:30 -0600

    front-index-refact: alert validation to show messages

--- Frontend/assets/js/index.js
+++ Frontend/assets/js/index.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
     const emailResult = sessionStorage.getItem("email-operation");
     const emailStatus = sessionStorage.getItem("email-status");
+    const accountResult = sessionStorage.getItem("eliminated-account");
+    const sessionResult = sessionStorage.getItem("not-account");
+    const closedSessionResult = sessionStorage.getItem("closed-session");
 
     const alertElement = $("#alert");
-    alertElement.hide();
 
     if(emailResult !== null && emailStatus !== null) {
         alertElement.text(emailResult);
@@ -19,25 +21,17 @@ $(document).ready(() => {
 
         sessionStorage.removeItem("email-operation");
         sessionStorage.removeItem("email-status");
-    }
-    
-    const accountStatus = sessionStorage.getItem("emliminated-account")
-
-    if(accountStatus === "true") {
-        alertElement.text("¡Cuenta Eliminada con Éxito!");
+    } else if(accountResult !== null || closedSessionResult !== null) {
+        alertElement.text(accountResult !== null ? accountResult : closedSessionResult);
         alertElement.slideDown(250);
         setTimeout(() => alertElement.slideUp(250, () => $(this).remove()), 5000);
-        sessionStorage.removeItem("emliminated-account");
-    }
-
-    const sessionStatus = sessionStorage.getItem("not-account")
-
-    if(sessionStatus === "true") {
+        sessionStorage.removeItem(accountResult !== null ? "eliminated-account" : "closed-session");
+    } else if(sessionResult !== null) {
         alertElement.removeClass("alert-success");
         alertElement.removeClass("text-success");
         alertElement.addClass("alert-danger");
         alertElement.addClass("text-danger");
-        alertElement.text("¡Necesitas Iniciar Sesión para Acceder a tu Perfil!");
+        alertElement.text(sessionResult);
         alertElement.slideDown(250);
         setTimeout(() => alertElement.slideUp(250, () => $(this).remove()), 5000);
         sessionStorage.removeItem("not-account");
