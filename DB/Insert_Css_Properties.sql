3556fc5cd396a932dc6efb759bcbcf95cfb155c6
Siria <sirifranco00@gmail.com>
2023-06-03 13:52:41 -0700

    back-products-feat: add css_properties sql

---
+++ DB/Insert_Css_Properties.sql
@@ -0,0 +1,28 @@
+USE petzonalize;
+
+SELECT * FROM petzonalize.css_properties;
+
+-- customizables 
+-- ID 1
+INSERT INTO css_properties (`dogTop`, `dogRight`, `catTop`, `catRight`)
+	VALUES ('17%', '10%', '27%', '5%' );
+    
+-- ID 2
+INSERT INTO css_properties (`dogTop`, `dogRight`, `catTop`, `catRight`)
+	VALUES ('15%', '15%', '20%', '5%' );
+
+-- ID 3
+INSERT INTO css_properties (`dogTop`, `dogRight`, `catTop`, `catRight`)
+	VALUES ('25%', '25%', '25%', '18%' );
+    
+-- ID 4
+INSERT INTO css_properties (`dogTop`, `dogRight`, `catTop`, `catRight`)
+	VALUES ('15%', '30%', '15%', '25%' );
+    
+-- ID 5
+INSERT INTO css_properties (`dogTop`, `dogRight`, `catTop`, `catRight`)
+	VALUES ('20%', '30%', '25%', '27%' );
+    
+-- ID 6
+INSERT INTO css_properties (`dogTop`, `dogRight`, `catTop`, `catRight`)
+	VALUES ('25%', '30%', '30%', '30%' );
