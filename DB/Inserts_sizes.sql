6ad15b48b229bd8796c710e913a2d4a60758586a
Luisin <luis.enrique.sanper@gmail.com>
2023-06-02 22:44:23 -0600

    back-db-feat: inserts sizes

---
+++ DB/Inserts_sizes.sql
@@ -0,0 +1,9 @@
+INSERT INTO sizes (`category`, `small`, `medium`, `big`)
+VALUES ("bowl", "Pequeño - 250 ml", "Mediano - 500 ml", "Grande - 1000 ml");
+
+INSERT INTO sizes (`category`, `small`, `medium`, `big`)
+VALUES ("nameplate", "Pequeña - 35 mm x 18 mm", "Mediana - 38 mm x 25 mm", "Grande - 50 mm x 32 mm");
+
+INSERT INTO sizes (`category`, `small`, `medium`, `big`)
+VALUES ("collar","Pequeño - 25 cm a 38 cm - 1.5cm de ancho", "Mediano - 32 cm a 48 cm - 2 cm de ancho","Grande - 39 cm a 59 cm - 2.5 cm de ancho");
+
