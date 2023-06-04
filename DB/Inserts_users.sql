d6765fa16f0fafac9e0333f844b6701df634a15c
Dennise A <sstteeffyy@hotmail.es>
2023-06-03 21:04:57 -0600

    back-products-feat: add users sql

---
+++ DB/Inserts_users.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Juan Fernando Reyes Sánchez", "juanreyssan@gmail.com", "525519673129" , "password", "admin");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Amaya Hernández Barrera", "mayitabar98@gmail.com", "525517909516" , "guestguest", "client");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("María Guadalupe Castellanos Córdoba", "lupita_osito32@gmail.com", "525580528301" , "qwertyuiop", "admin");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Darío González Pichardo", "darioastronautagp@gmail.com", "525527839796", "col12345", "client");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Manuel Rodríguez González", "manu93rzgz@gmail.com", "525518090880" , "megustaelarroz18", "admin");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Alexis Peralta Muñoz", "alpemu_electro22@gmail.com", "525542481533", "alexisperalta22", "client");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Cristina Espinoza Leyva", "kristyespinoza921@gmail.com", "525573406189", "footballkristy92", "admin");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Isidro Juárez Velázquez", "juarezsd29473@gmail.com", "525569771680", "dragonisidro123", "client");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Juana Sánchez Armenta", "jusanarmenti21@gmail.com", "525569753092", "princessjuanita77", "admin");
+
+INSERT INTO users (`name`, `email`, `phone`, `password`, `privileges`)
+VALUES 	("Sandra Castillo Pérez", "sandraexploradora21@gmail.com", "525546483824", "passw0rdstarwars21", "client");
