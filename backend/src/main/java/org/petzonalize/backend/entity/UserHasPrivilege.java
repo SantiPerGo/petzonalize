f5662bc6f5f135205cc9c06f02b698f4c30aa48c
Santiago Pérez Gómez <spergo@outlook.com>
2023-06-09 12:57:52 -0600

    back-entity-fix: added primary key to connection entities

--- backend/src/main/java/org/petzonalize/backend/entity/UserHasPrivilege.java
+++ backend/src/main/java/org/petzonalize/backend/entity/UserHasPrivilege.java
@@ -1,6 +1,10 @@
 package org.petzonalize.backend.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -14,6 +18,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity(name="users_has_privileges")
 public class UserHasPrivilege {
+	@Id
+	@Column(name = "users_has_privileges_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@ManyToOne
     @JoinColumn(name = "user_id")
