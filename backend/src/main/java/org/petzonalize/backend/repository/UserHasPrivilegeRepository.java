ca8b7f45089a05b923057ca707e1011f73c30d4a
Santiago Pérez Gómez <spergo@outlook.com>
2023-06-12 17:05:34 -0600

    back-service-fix: user login returns user with privileges

--- backend/src/main/java/org/petzonalize/backend/repository/UserHasPrivilegeRepository.java
+++ backend/src/main/java/org/petzonalize/backend/repository/UserHasPrivilegeRepository.java
@@ -1,8 +1,11 @@
 package org.petzonalize.backend.repository;
 
+import java.util.Optional;
+
+import org.petzonalize.backend.entity.User;
 import org.petzonalize.backend.entity.UserHasPrivilege;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserHasPrivilegeRepository extends JpaRepository<UserHasPrivilege, Long> {
-
+	Optional<UserHasPrivilege> findByUser(User user);
 }
