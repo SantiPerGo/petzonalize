7fbc6e850c4944b783e7091c315a7372ef27513c
Siria <sirifranco00@gmail.com>
2023-06-13 23:24:52 -0700

    front-login-fix: merge file

--- Frontend/assets/js/login.js
+++ Frontend/assets/js/login.js
@@ -40,17 +40,41 @@ buttonBackToLogin.addEventListener('click', ()=>{
     clearInputs();
 });
 
-//----------------Show in console list of users registered---------------------
-fetch('https://petzonalize.up.railway.app/users')
-  .then(response => response.json())
+//----------------Show in console list of users registered from Database or Users.Json---------------------
+  fetch('https://petzonalize.up.railway.app/users')
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to fetch data from the API');
+    }
+  })
   .then(users => {
     console.log(users);
+    console.log("usuarios obtenidos de la Data Base");
   })
   .catch(error => {
     console.error('Error:', error);
+    // Fetch from local JSON file if there's an error
+    fetch('/assets/json/users.json')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Failed to fetch data from the local JSON file');
+        }
+      })
+      .then(users => {
+        console.log(users);
+        console.log("usuarios obtenidos de archivo local JSON");
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   });
 
-// ------------Look if there's a user registered in local storage
+
+// ------------Look if there's a user registered in local storage--------------
  $(document).ready(() => {
     // Forms validation
     validateForm(signUpForm);
@@ -59,8 +83,11 @@ fetch('https://petzonalize.up.railway.app/users')
     
     // Verifying if user exists
     let user = localStorage.getItem("users-logged-in");
-    if(user != null)
-    window.location.href = 'profile.html';
+    if(user != null){
+        window.location.href = 'profile.html';
+    } else {
+        console.log("sesión aún no iniciada")
+    }
 });
 
 //--------- Limpiar campos --------------
@@ -81,12 +108,9 @@ const resetInput = input => {
     $(`#${input.id}-error`).remove();
 };
 
-// ------ Escucha cuando el usuario inicie sesion -----
-loginForm.submit(submitButton => {
+// ------ Escucha cuando el usuario inicie sesion con DB -----
+/* loginForm.submit(submitButton => {
     submitButton.preventDefault();
-
-    /* const email = $("#input-email-login").val().trim();
-    const password = $("#input-password-login").val().trim(); */
 
     const user = {
         email: $("#input-email-login").val().trim(),
@@ -116,9 +140,70 @@ loginForm.submit(submitButton => {
             })
             .catch(error =>{
                 console.log(error);
-            })
+            }) 
+}); */
+
+// ---------- Inicio de sesión con DB ó Json local -------------
+loginForm.submit(submitButton => {
+    submitButton.preventDefault();
+
+    const user = {
+        email: $("#input-email-login").val().trim(),
+        password: $("#input-password-login").val().trim()
+    };
+
+    if (loginForm.valid()) {
+        fetch("https://petzonalize.up.railway.app/users/login", {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else if (response.status === 400) {
+                showAlert("Correo o contraseña incorrectos");
+            } else if (response.status === 404) {
+                showAlert("La cuenta de usuario no existe");
+            }
+
+            throw new Error("Login failed");
+        })
+        .then(usersResponse => {
+            localStorage.setItem(`users-logged-in`, JSON.stringify(usersResponse));
+            window.location.href = 'profile.html';
+            console.log("Sesión iniciada");
+        })
+        .catch(error => {
+            console.log(error);
+            // Fetch from local JSON file for email and password validation
+            fetch("/assets/json/users.json")
+                .then(response => {
+                    if (response.ok) {
+                        console.log("No se puede acceder a la base de datos, se está intentando ingresar localmente");
+                        return response.json();
+                    } else {
+                        throw new Error("Failed to fetch from local JSON file");
+                    }
+                })
+                .then(usersResponse => {
+                    const matchedUser = usersResponse.find(
+                        u => u.email === user.email && u.password === user.password
+                    );
+                    if (matchedUser) {
+                        localStorage.setItem(`users-logged-in`, JSON.stringify(matchedUser));
+                        window.location.href = 'profile.html';
+                        console.log("Sesión iniciada desde archivo JSON local");
+                    } else {
+                        showAlert("Correo o contraseña incorrectos");
+                    }
+                })
+                .catch(error => console.log(error));
+        });
+    }
 });
 
+<<<<<<< HEAD
 const loadAlertText = (text, type) => {
     const toastElement = $("#toast");
     const toastInstance = bootstrap.Toast.getOrCreateInstance(toastElement);
@@ -126,6 +211,18 @@ const loadAlertText = (text, type) => {
     toastBody.text(text);
     toastElement.addClass(`toast-${type}`);
     toastInstance.show();
+=======
+// ----- Alertas -------
+const showAlert = text => {
+    alertElement.text(text);
+    alertElement.slideDown(250);
+    setTimeout(() => alertElement.slideUp(250, () => $(this).remove()), 5000);
+
+    alertElement.removeClass("alert-success");
+    alertElement.removeClass("text-success");
+    alertElement.addClass("alert-danger");
+    alertElement.addClass("text-danger");
+>>>>>>> 1297482 (front-login-perf: login n register fetch with database n local json)
 };
 
 // ----- Escucha cuando el usuario recupera contraseña ------
@@ -145,6 +242,9 @@ recoverForm.submit(submitButton => {
     )} 
 });
 
+// ------ Escucha cuando el usuario recupera contraseña del Database o Users.Json -------------
+
+
 // ----- Escucha cuando el usuario se registre ------
 signUpForm.submit(submitButton => {
     submitButton.preventDefault();
@@ -154,7 +254,7 @@ signUpForm.submit(submitButton => {
 });
 
 //------------Make a POST request to create the user account
-const creatingUserAccount = () => {
+/* const creatingUserAccount = () => {
     const name = document.getElementById("input-name-register").value;
     const email = document.getElementById("input-email-register").value;
     const address = document.getElementById("input-address-register").value;
@@ -194,6 +294,77 @@ const creatingUserAccount = () => {
     .catch(error => {
         console.error("Not user registered:", error);
         
-    });
+    });    
+} */
+
+//------------Make a POST request to create the user account in Database or Users.Json --------------
+const creatingUserAccount = () => {
+    const name = document.getElementById("input-name-register").value;
+    const email = document.getElementById("input-email-register").value;
+    const address = document.getElementById("input-address-register").value;
+    const phone = document.getElementById("input-phone-register").value;
+    const password = document.getElementById("input-signup-password").value;
+
+    const user = {
+        name: name,
+        address: address,
+        email: email,
+        phone: phone,
+        password: password,
+        privileges: {
+            id: 2,
+            privilege: "client"
+        } 
+    };
     
-}
+    fetch("https://petzonalize.up.railway.app/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => {
+        if (response.ok) 
+            return response.json();
+        else 
+            console.log("No se puede acceder a la base de datos, se creará localmente");
+            throw new Error("Failed to create account. Status: " + response.status);
+    })
+    .then(response => {
+        localStorage.setItem("users-logged-in", JSON.stringify(response));
+        window.location.href = 'profile.html';
+        console.log("Cuenta creada");
+    })
+    .catch(error => {
+        console.error("Not user registered:", error);
+        // Fetch from local JSON file if the API request fails
+        fetch("/assets/json/users.json")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("Error al acceder al archivo local JSON");
+                }
+            })
+            .then(usersResponse => {
+                // Check if user already exists in the local JSON data
+                const existingUser = usersResponse.find(
+                    u => u.email === user.email
+                );
+                if (existingUser) {
+                    console.error("This email already exists:", existingUser);
+                    showAlert("Este correo ya existe");
+                } else {
+                    // Add the new user to the local JSON data
+                    usersResponse.push(user);
+                    localStorage.setItem("users-logged-in", JSON.stringify(user));
+                    window.location.href = 'profile.html';
+                    console.log("User created in local JSON file:", user);
+                    // Proceed with further logic or redirection
+                }
+            })
+            .catch(error => console.log(error));
+    });    
+};
+
